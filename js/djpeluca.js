371b4a4bfc7361b8aa65b176c0a7166fec617bc9
djpeluca <adrianbs@gmail.com>
2014-03-30 17:11:37 -0300

    primer

---
+++ js/djpeluca.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    var visible;
+    //tipsy tooltip
+    $('#djpeluca').tipsy({html: true, gravity: 'w' });
+    
+    //title and subtitle hover changes
+    $('#subtitle').hover(function() { 
+        $('#subtitle').text('I do a lot of stuff!');
+    },function(){ 
+        $('#subtitle').text('Comunicate & compute');
+    });
+    $('#title').hover(function() { 
+        $('#title').text('Adrian Barrera');
+    },function(){ 
+        $('#title').text('DJ Peluca');
+    });
+    //tipsy tooltip and hover changes
+	
+	//check query for tab
+		var visible;
+		var querystring = window.location.search;
+		if (querystring != ""){
+			var splited = new Array();
+			splitted = querystring.split('=');
+			visible = splitted[1];
+			$("html, body").animate({ scrollTop: $("#divmenu").position().top }, 1000);
+		}
+	//check query for tab  
+    
+    //menu tabs
+    $('ul#menu li.'+visible).addClass('select');
+		$('#' + visible).removeClass('none');
+    
+    $('ul#menu li').click(function(){
+        var Classes = $(this).attr('class').split(" ");
+        $('ul#menu li.'+visible).removeClass('select');
+        $(this).addClass('select');
+        if( visible!= Classes[0])
+        {
+            $('#' + Classes[0]).removeClass('none');
+            $('#' +  visible).addClass('none');
+            visible = Classes[0];
+			var query = "?tab="+visible;
+			window.history.pushState("object or string", "DJ PELUCA", query);
+        }
+    });
+    //menu tabs
+    
+    //scroll on tab click
+    $('.tab').click(function(){
+        $("html, body").animate({ scrollTop: $("#divmenu").position().top }, 1000);
+    });
+    //scroll on tab click
+	
+	 
+});
